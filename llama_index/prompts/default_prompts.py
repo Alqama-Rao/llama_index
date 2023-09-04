84fe997a01a1d7d4c9bde51388cafc28d5d5d70e
Alqama-Rao <alqamarao89@gmail.com>
2023-09-05 02:53:42 +0500

    Default prompt updated
    Default prompt updated

--- llama_index/prompts/default_prompts.py
+++ llama_index/prompts/default_prompts.py
@@ -270,8 +270,8 @@ DEFAULT_REFINE_TABLE_CONTEXT_PROMPT = PromptTemplate(
 
 DEFAULT_KG_TRIPLET_EXTRACT_TMPL = (
     "Some text is provided below. Given the text, extract up to "
-    "{max_knowledge_triplets} "
-    "knowledge triplets in the form of (subject, predicate, object). Avoid stopwords.\n"
+    "{max_knowledge_triplets}"
+    "knowledge triplets in the form of (subject, predicate, object).Only include relevant entities. Avoid stopwords.\n"
     "---------------------\n"
     "Example:"
     "Text: Alice is Bob's mother."
